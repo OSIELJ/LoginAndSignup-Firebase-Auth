267fd6c230c2ac17bc1d388322be066563dcf948
OSIELJ <osieljuniormartins@gmail.com>
2024-06-15 21:45:59 -0300

    Criação a lógica para logar  usuário na LoginActivity.kt usando Firebase Auth.

--- app/src/main/java/com/example/loginsignupfirebaseauth/LoginActivity.kt
+++ app/src/main/java/com/example/loginsignupfirebaseauth/LoginActivity.kt
@@ -1,20 +1,55 @@
 package com.example.loginsignupfirebaseauth
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.loginsignupfirebaseauth.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.btnLogin.setOnClickListener {
+            val email = binding.edtEmail.text.toString()
+            val senha = binding.edtPassword.text.toString()
+
+            if (email.isNotEmpty() && senha.isNotEmpty()){
+
+                firebaseAuth.signInWithEmailAndPassword(email, senha).addOnCompleteListener {
+
+                    if (it.isSuccessful){
+
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                    }else{
+                        Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }else{
+                Toast.makeText(this, "Campos não podem ser vazios", Toast.LENGTH_SHORT).show()
+
+            }
+        }
+        binding.tvToGoSignup.setOnClickListener {
+            val signupIntent = Intent(this, SignupActivity::class.java)
+            startActivity(signupIntent)
         }
     }
 }
